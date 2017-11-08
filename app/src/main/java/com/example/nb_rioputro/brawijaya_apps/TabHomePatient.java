1e577e0d2c66e8d9cd8cae35cae81c22f50cf394
nb-rioputro <riotrilaksonop@gmail.com>
2017-11-08 09:52:26 +0700

    change tab item

--- app/src/main/java/com/example/nb_rioputro/brawijaya_apps/TabHomePatient.java
+++ app/src/main/java/com/example/nb_rioputro/brawijaya_apps/TabHomePatient.java
@@ -5,17 +5,19 @@ package com.example.nb_rioputro.brawijaya_apps;
  */
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class TabHomePatient extends Fragment {
-    Button btnFoodOrder;
+    Button btnFoodOrder, btnLogout;
     String mId;
 
     @Override
@@ -31,6 +33,15 @@ public class TabHomePatient extends Fragment {
             }
         });
 
+        btnLogout = (Button) rootView.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userLogout();
+            }
+        });
+
+
         return rootView;
     }
 
@@ -38,5 +49,38 @@ public class TabHomePatient extends Fragment {
         Intent orderIntent = new Intent(getActivity(), FoodOrderActivity.class);
         startActivity(orderIntent);
 
+    }
+
+    private void userLogout() {
+        AlertDialog.Builder adLogout = new AlertDialog.Builder(getActivity());
+        adLogout.setMessage("Are you sure you want to logout? ");
+        adLogout.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                SharedPreferences spLogout = getActivity().getSharedPreferences(Config.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = spLogout.edit();
+
+                editor.putBoolean(Config.LOGGEDIN_SHARED_PREF, false);
+                editor.putString(Config.USERNAME_SHARED_PREF, "");
+                editor.putString(Config.NAME_SHARED_PREF, "");
+                editor.putString(Config.ROLE_SHARED_PREF, "");
+
+                editor.commit();
+
+                Intent logoutIntent = new Intent(getActivity(), LoginActivity.class);
+                logoutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(logoutIntent);
+            }
+        });
+
+        adLogout.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog alertDialog = adLogout.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/example/nb_rioputro/brawijaya_apps/TabOptionsPatient.java
+++ app/src/main/java/com/example/nb_rioputro/brawijaya_apps/TabOptionsPatient.java
@@ -16,58 +16,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class TabOptionsPatient extends Fragment{
-    Button btnLogout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_options_patient, container, false);
-        btnLogout = (Button)rootView.findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                userLogout();
-            }
-        });
-
 
 
         return rootView;
 
-
-
     }
 
-    private void userLogout() {
-        AlertDialog.Builder adLogout = new AlertDialog.Builder(getActivity());
-        adLogout.setMessage("Are you sure you want to logout? ");
-        adLogout.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                SharedPreferences spLogout = getActivity().getSharedPreferences(Config.SHARED_PREF_NAME, Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = spLogout.edit();
 
-                editor.putBoolean(Config.LOGGEDIN_SHARED_PREF, false);
-                editor.putString(Config.USERNAME_SHARED_PREF, "");
-                editor.putString(Config.NAME_SHARED_PREF, "");
-                editor.putString(Config.ROLE_SHARED_PREF, "");
-
-                editor.commit();
-
-                Intent logoutIntent = new Intent(getActivity(), LoginActivity.class);
-                logoutIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(logoutIntent);
-            }
-        });
-
-        adLogout.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-
-        AlertDialog alertDialog = adLogout.create();
-        alertDialog.show();
-    }
 }
